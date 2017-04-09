445e3bf9063f36b4acc566c1e970b6dbcfbc341a
prat-degwekar <prat.degwekar@gmail.com>
2017-04-09 19:50:28 +0530

    updated normalization building blocks

--- normal.cpp
+++ normal.cpp
@@ -5,6 +5,9 @@ using namespace std;
 bool fdl[100][100];					//fdl[i][j] represents attribute j+1 is on LHS of fd no. i
 bool fdr[100][100];
 
+bool fdlt[100][100];				//temporary sets
+bool fdrt[100][100];
+
 int checkvis(bool *vis , int s)						//gets if all reachable					#works
 {
 	for(int i=0 ; i<s ; i++)
@@ -13,6 +16,78 @@ int checkvis(bool *vis , int s)						//gets if all reachable					#works
 			return 0;
 	}
 	return 1;
+}
+
+bool equl(bool *b1 , bool *b2 , int n)				//true if b1 = b2
+{
+	for(int i=0 ; i<n ; i++)
+	{
+		if(b1[i]!=b2[i])
+			return false;
+	}
+	
+	return true;
+}
+
+void cop(bool *src , bool *dest , int n)							//copies source visibility matrix into dest. one...
+{
+	for(int i=0 ; i<n ; i++)
+		dest[i] = src[i];
+	
+}
+
+void cop_d(bool **fdl , bool **fdr , bool **fdlt , bool **fdrt , int n , int m)								//copies fdl into fdlt and fdr into fdrt
+{
+	for(int i=0 ; i<m ; i++)
+	{
+		for(int j=0 ; j<n ; j++)
+		{
+			fdlt[i][j] = fdl[i][j];
+			fdrt[i][j] = fdr[i][j];
+		}
+	}
+}
+
+bool* closure(bool **fdl , bool **fdr , int *list , int s , int n , int m)						//gets closure of fd set and returns vis
+{
+	bool *vis = new bool[n];
+	bool *temp = new bool[n];
+	for(int i=0 ; i<n ; i++)
+		vis[i] = 0;
+	for(int i=0 ; i<s ; i++)
+		vis[list[i]] = 1;
+	
+	cop(vis , temp , n);
+	
+	do
+	{
+		for(int i=0 ; i<m ; i++)
+		{
+			for(int j=0 ; j<n ; j++)
+			{
+				bool flg = 0;
+				if(fdl[i][j] == 1)
+				{	
+					if(temp[j] == 1)
+						continue;
+					else
+					{
+						flg = 1;
+						break;
+					}
+				}
+				if(flg == 1)
+					break;
+				
+				for(int j=0 ; j<n ; j++)
+					if(fdr[i][j] == 1)
+						temp[j] = 1;
+			}
+		}
+	}while(!equl(vis , temp , n));
+	
+	return vis;
+	
 }
 
 int reach(int *list , int s , int n , int m)		//s is size of list of attributes , n is no. of attr. , m is no. of fds			returns 1 if all reachable from <list>
@@ -157,6 +232,108 @@ void ckeycheck(int n , int m)					//what all min need to be there				#works
 	//length = ind ++
 
 	//generate l+1 subs
+
+}
+
+bool equiv(bool **fdl , bool **fdr , bool **fdlt , bool **fdrt , int n , int m)				//full equivalence check  ->  finished working 'z' state		true if equivalent
+{
+	bool *v1 = new bool[n];
+	bool *v2 = new bool[n];
+
+	int *list = new int[n];
+	bool fl = 1;
+
+	for(int i=0 ; i<n ; i++)
+	{
+		list[0] = i;
+		v1 = closure(fdl , fdr , list , 1 , n , m);
+		v2 = closure(fdlt , fdrt , list , 1 , n , m);
+		fl = equl(v1 , v2 , n);
+		if(!fl)
+			break;
+	}
+	if(!fl)																						//after checking reach of all attributes , if discrepancy
+		return false;
+
+	//get for all lhs
+
+	int in = 0;
+	fl = 1;
+	for(int i = 0 ; i<m ; i++)
+	{
+		in = 0;
+		for(int j=0 ; j<n ; j++)
+			if(fdl[i][j] == 1)
+				list[in++] = j;
+
+		v1 = closure(fdl , fdr , list , in , n , m);
+		v2 = closure(fdlt , fdrt , list , in , n , m);
+		fl = equl(v1 , v2 , n);
+		if(!fl)
+			break;
+	}
+	if(!fl)
+		return false;
+
+	return true;
+}
+
+void mincov(bool **fdl , bool **fdr , int n , int m)									//does brute min cover and puts minimum cover fd set into fdl and fdr
+{
+	bool **fdlt = new bool*[n];          //declare graph
+
+  	for(int i = 0 ; i<n ; i++)       //declare graph
+  		fdlt[i] = new bool[n];
+
+    bool **fdrt = new bool*[n];          //declare graph
+
+    for(int i = 0 ; i<n ; i++)       //declare graph
+    	fdrt[i] = new bool[n];
+
+    cop_d(fdl , fdr , fdlt , fdrt , n , m);
+    int cnt = 0;
+    bool fin = 0;
+    do   													//repeatedly removes all possible attributes and checks for equivalence over all fds and breaks when no change at all possible any more
+    {
+    	fin = 0;
+    	for(int i=0 ; i<m ; i++)
+    	{
+    		cnt = 0;
+    		for(int k=0 ; k<n ; k++)
+    		{
+    			int cmp = cnt;
+	    		for(int j=0 ; j<n ; j++)
+    			{
+		    		if(fdl[i][j] == 1)
+    				{
+    					if(cmp == 0)
+		    			{
+		    				fdlt[i][j] = 0;					//remove i from fdlt[i]
+    						break;
+    					}
+    					else
+    						cmp--;
+    				}
+    			}
+
+    			if(cmp > 0)
+    				break;
+
+    			bool k =equiv(fdl , fdr , fdlt , fdrt , n , m);
+    			if(k)
+	    		{
+		    		cop_d(fdlt , fdrt , fdl , fdr , n , m);						//if equivalent , change
+	    			fin = 1;
+	    			break;
+	    		}
+    			else
+		    		cop_d(fdl , fdr , fdlt , fdrt , n , m);						//else revert
+		    	cnt++;
+		    }
+	    }
+	}while(fin);
+
+	//fdl and fdr have min cover fd sets
 
 }
 
